b585052f953c834ce96b8a295fcf2c1647db84e4
Slavagf <viacheslavgf@gmail.com>
2023-05-16 14:51:16 +0300

    12312

--- SlavaWork.tab/Concrete Table.panel/Concrete.pushbutton/actual_DynamoCode.script.py
+++ SlavaWork.tab/Concrete Table.panel/Concrete.pushbutton/actual_DynamoCode.script.py
@@ -678,104 +678,107 @@ wall_collector = FilteredElementCollector(doc). \
 
 
 def getting_Area_Volume_walls(walls_list):
-    for wall in wall_collector:
-        new_w = doc.GetElement(wall)
-        # going to WallType
-        try:
-            wall_type = new_w.WallType
-        except:
-            pass
-        volume_param = new_w.LookupParameter("Volume")
-        area_param = new_w.LookupParameter("Area")
-        wall_duplicationTypeMark = wall_type.LookupParameter("Duplication Type Mark").AsString()
-        wall_type_comments = wall_type.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_COMMENTS).AsString()
+    try:
+        for wall in wall_collector:
+            new_w = doc.GetElement(wall)
+            # going to WallType
+            try:
+                wall_type = new_w.WallType
+            except:
+                pass
+            volume_param = new_w.LookupParameter("Volume")
+            area_param = new_w.LookupParameter("Area")
+            wall_duplicationTypeMark = wall_type.LookupParameter("Duplication Type Mark").AsString()
+            wall_type_comments = wall_type.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_COMMENTS).AsString()
 
-        if wall_duplicationTypeMark == "Slurry Bisus":
-            key = "Slurry Bisus/קיר סלארי ביסוס"
-            if key in slurry_Bisus:
-                wall_area = area_param.AsDouble() * 0.092903
-                wall_volume = volume_param.AsDouble() * 0.0283168466
-                slurry_Bisus[key]["Area"] += wall_area
-                slurry_Bisus[key]["Volume"] += wall_volume
-            elif key not in slurry_Bisus:
-                wall_area = area_param.AsDouble() * 0.092903
-                wall_volume = volume_param.AsDouble() * 0.0283168466
-                slurry_Bisus[key] = {"Area": wall_area, "Volume": wall_volume}
-
-        if wall_duplicationTypeMark == "Slurry Dipun":
-            key = "Slurry Dipun/קיר סלארי דיפון"
-            if key in slurry_Dipun:
-                wall_area = area_param.AsDouble() * 0.092903
-                wall_volume = volume_param.AsDouble() * 0.0283168466
-                slurry_Dipun[key]["Area"] += wall_area
-                slurry_Dipun[key]["Volume"] += wall_volume
-            elif key not in slurry_Dipun:
-                wall_area = area_param.AsDouble() * 0.092903
-                wall_volume = volume_param.AsDouble() * 0.0283168466
-                slurry_Dipun[key] = {"Area": wall_area, "Volume": wall_volume}
-
-        if wall_duplicationTypeMark == "Demolished":
-            key = "Demolished/נהרס"
-            if key not in walls_in_new:
-                wall_area = area_param.AsDouble() * 0.092903
-                wall_volume = volume_param.AsDouble() * 0.0283168466
-                walls_in_new[key] = {"Area": wall_area, "Volume": wall_volume}
-            elif key in walls_in_new:
-                wall_area = area_param.AsDouble() * 0.092903
-                wall_volume = volume_param.AsDouble() * 0.0283168466
-                walls_in_new[key]["Area"] += wall_area
-                walls_in_new[key]["Volume"] += wall_volume
-
-        if wall_duplicationTypeMark == "Existing":
-            key = "Existing/קיים"
-            if key not in walls_in_new:
-                wall_area = area_param.AsDouble() * 0.092903
-                wall_volume = volume_param.AsDouble() * 0.0283168466
-                walls_in_new[key] = {"Area": wall_area, "Volume": wall_volume}
-            elif key in walls_in_new:
-                wall_area = area_param.AsDouble() * 0.092903
-                wall_volume = volume_param.AsDouble() * 0.0283168466
-                walls_in_new[key]["Area"] += wall_area
-                walls_in_new[key]["Volume"] += wall_volume
-
-        if wall_duplicationTypeMark in ["Concrete", "Concrete-WR", "Concrete-P"]:
-            if wall_type_comments == "FrIN":
-                wall_key = "קירות פנימיים מבטון/Walls-In"
-                if wall_key not in walls_in_new:
+            if wall_duplicationTypeMark == "Slurry Bisus":
+                key = "Slurry Bisus/קיר סלארי ביסוס"
+                if key in slurry_Bisus:
                     wall_area = area_param.AsDouble() * 0.092903
                     wall_volume = volume_param.AsDouble() * 0.0283168466
-                    walls_in_new[wall_key] = {"Area": wall_area, "Volume": wall_volume}
-                elif wall_key in walls_in_new:
+                    slurry_Bisus[key]["Area"] += wall_area
+                    slurry_Bisus[key]["Volume"] += wall_volume
+                elif key not in slurry_Bisus:
                     wall_area = area_param.AsDouble() * 0.092903
                     wall_volume = volume_param.AsDouble() * 0.0283168466
-                    walls_in_new[wall_key]["Area"] += wall_area
-                    walls_in_new[wall_key]["Volume"] += wall_volume
+                    slurry_Bisus[key] = {"Area": wall_area, "Volume": wall_volume}
 
-        if wall_duplicationTypeMark in ["Concrete", "Concrete-WR", "Concrete-P"]:
-            if wall_type_comments == "FrOut":
-                wall_key = "קירות חוץ מבטון/Walls-Out"
-                if wall_key not in walls_out_new:
+            if wall_duplicationTypeMark == "Slurry Dipun":
+                key = "Slurry Dipun/קיר סלארי דיפון"
+                if key in slurry_Dipun:
                     wall_area = area_param.AsDouble() * 0.092903
                     wall_volume = volume_param.AsDouble() * 0.0283168466
-                    walls_out_new[wall_key] = {"Area": wall_area, "Volume": wall_volume}
-                elif wall_key in walls_out_new:
+                    slurry_Dipun[key]["Area"] += wall_area
+                    slurry_Dipun[key]["Volume"] += wall_volume
+                elif key not in slurry_Dipun:
                     wall_area = area_param.AsDouble() * 0.092903
                     wall_volume = volume_param.AsDouble() * 0.0283168466
-                    walls_out_new[wall_key]["Area"] += wall_area
-                    walls_out_new[wall_key]["Volume"] += wall_volume
+                    slurry_Dipun[key] = {"Area": wall_area, "Volume": wall_volume}
 
-        if wall_duplicationTypeMark == "Precast":
-            if wall_type_comments == "FrOut":
-                wall_key = "קירות טרומיים/Concrete-Precast"
-                if wall_key not in precast_elements:
+            if wall_duplicationTypeMark == "Demolished":
+                key = "Demolished/נהרס"
+                if key not in walls_in_new:
                     wall_area = area_param.AsDouble() * 0.092903
                     wall_volume = volume_param.AsDouble() * 0.0283168466
-                    precast_elements[wall_key] = {"Area": wall_area, "Volume": wall_volume}
-                elif wall_key in precast_elements:
+                    walls_in_new[key] = {"Area": wall_area, "Volume": wall_volume}
+                elif key in walls_in_new:
                     wall_area = area_param.AsDouble() * 0.092903
                     wall_volume = volume_param.AsDouble() * 0.0283168466
-                    precast_elements[wall_key]["Area"] += wall_area
-                    precast_elements[wall_key]["Volume"] += wall_volume
+                    walls_in_new[key]["Area"] += wall_area
+                    walls_in_new[key]["Volume"] += wall_volume
+
+            if wall_duplicationTypeMark == "Existing":
+                key = "Existing/קיים"
+                if key not in walls_in_new:
+                    wall_area = area_param.AsDouble() * 0.092903
+                    wall_volume = volume_param.AsDouble() * 0.0283168466
+                    walls_in_new[key] = {"Area": wall_area, "Volume": wall_volume}
+                elif key in walls_in_new:
+                    wall_area = area_param.AsDouble() * 0.092903
+                    wall_volume = volume_param.AsDouble() * 0.0283168466
+                    walls_in_new[key]["Area"] += wall_area
+                    walls_in_new[key]["Volume"] += wall_volume
+
+            if wall_duplicationTypeMark in ["Concrete", "Concrete-WR", "Concrete-P"]:
+                if wall_type_comments == "FrIN":
+                    wall_key = "קירות פנימיים מבטון/Walls-In"
+                    if wall_key not in walls_in_new:
+                        wall_area = area_param.AsDouble() * 0.092903
+                        wall_volume = volume_param.AsDouble() * 0.0283168466
+                        walls_in_new[wall_key] = {"Area": wall_area, "Volume": wall_volume}
+                    elif wall_key in walls_in_new:
+                        wall_area = area_param.AsDouble() * 0.092903
+                        wall_volume = volume_param.AsDouble() * 0.0283168466
+                        walls_in_new[wall_key]["Area"] += wall_area
+                        walls_in_new[wall_key]["Volume"] += wall_volume
+
+            if wall_duplicationTypeMark in ["Concrete", "Concrete-WR", "Concrete-P"]:
+                if wall_type_comments == "FrOut":
+                    wall_key = "קירות חוץ מבטון/Walls-Out"
+                    if wall_key not in walls_out_new:
+                        wall_area = area_param.AsDouble() * 0.092903
+                        wall_volume = volume_param.AsDouble() * 0.0283168466
+                        walls_out_new[wall_key] = {"Area": wall_area, "Volume": wall_volume}
+                    elif wall_key in walls_out_new:
+                        wall_area = area_param.AsDouble() * 0.092903
+                        wall_volume = volume_param.AsDouble() * 0.0283168466
+                        walls_out_new[wall_key]["Area"] += wall_area
+                        walls_out_new[wall_key]["Volume"] += wall_volume
+
+            if wall_duplicationTypeMark == "Precast":
+                if wall_type_comments == "FrOut":
+                    wall_key = "קירות טרומיים/Concrete-Precast"
+                    if wall_key not in precast_elements:
+                        wall_area = area_param.AsDouble() * 0.092903
+                        wall_volume = volume_param.AsDouble() * 0.0283168466
+                        precast_elements[wall_key] = {"Area": wall_area, "Volume": wall_volume}
+                    elif wall_key in precast_elements:
+                        wall_area = area_param.AsDouble() * 0.092903
+                        wall_volume = volume_param.AsDouble() * 0.0283168466
+                        precast_elements[wall_key]["Area"] += wall_area
+                        precast_elements[wall_key]["Volume"] += wall_volume
+    except:
+        pass
 
 
 getting_Area_Volume_walls(wall_collector)
